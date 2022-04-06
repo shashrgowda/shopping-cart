68c2264215ec85606abfe63f3933ccf4a0264b73
shashrgowda <shashankr6@gmail.com>
2022-04-06 23:52:05 +0530

    buy 6 for the price of 5 discount logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,31 +8,52 @@ import CartItem from "./CartItem/CartItem";
 const Cart = ({ cart }) => {
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalItems, setTotalItems] = useState(0);
+  const [totalActualAmount, setTotalActualAmount] = useState(0);
+  const [totalDiscountPrice, setTotalDiscountAmount] = useState(0);
 
   useEffect(() => {
     let items = 0;
     let price = 0;
+    let actualPrice = 0;
 
     cart.forEach((item) => {
       items += item.qty;
+      actualPrice += item.qty * item.price;
       const itemTotal = item.price * item.qty;
       const threshold = item.qty / item.discountQty;
-      const discountAmt = itemTotal - totalPrice;
+      const quotient = Math.floor(item.qty / item.discountQty);
 
       if (item.id === 1) {
         price +=
-          item.qty % 2 === 0
+          item.qty % item.discountQty === 0
             ? itemTotal - item.qty + threshold
             : itemTotal - item.qty + threshold + item.price - item.discountQty;
       } else {
         price +=
-          item.qty % 6 === 0 ? itemTotal - item.price : itemTotal + item.price;
+          item.qty % item.discountQty === 0
+            ? itemTotal - item.price
+            : (itemTotal - item.price * (item.qty - 1)) * item.qty -
+              item.price * quotient;
       }
     });
 
+    const discountedAmt = totalActualAmount - totalPrice;
+
     setTotalItems(items);
     setTotalPrice(price);
-  }, [cart, totalPrice, totalItems, setTotalPrice, setTotalItems]);
+    setTotalActualAmount(actualPrice);
+    setTotalDiscountAmount(discountedAmt);
+  }, [
+    cart,
+    totalPrice,
+    totalItems,
+    totalActualAmount,
+    totalDiscountPrice,
+    setTotalPrice,
+    setTotalItems,
+    setTotalActualAmount,
+    setTotalDiscountAmount,
+  ]);
 
   return (
     <div className="cart">
@@ -46,6 +67,10 @@ const Cart = ({ cart }) => {
         <div className="summary__price">
           <span>TOTAL: ({totalItems} items)</span>
           <span>£ {totalPrice.toFixed(2)}</span>
+        </div>
+        <div className="summary__price">
+          <span>TOTAL DISCOUNT: </span>
+          <span>£ {totalDiscountPrice.toFixed(2)}</span>
         </div>
         <button className="summary__checkoutBtn">Proceed To Checkout</button>
       </div>
